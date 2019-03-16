dc103208b2cc436a6b2450cc21f5d243269d394a
nickisobrien <nickisobrien@gmail.com>
2019-03-16 20:03:26 +0000

    moved tools up a dir and added grub src

--- common.sh
+++ common.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PREFIX="$HOME/tools/cross"
+PREFIX="$HOME/tools"
 TARGET=i686-elf
 PATH="$PREFIX/bin:$PATH"
 
 BINUTILS_SRC="$PREFIX/nOS-binutils-gdb"
 GCC_SRC="$PREFIX/nOS-gcc"
+GRUB_SRC="$PREFIX/nOS-grub"
